1b3f6be4ab68289d457aa2f626488b42e89980b8
WakameJP <61314385+WakameJP@users.noreply.github.com>
2020-02-24 19:18:31 +0900

    Hotdog Bot v1.4.2
    えっと...あの...すみませんでした。
    はい、GlobalChatバグだらけだったので一回撤収しました...
    それにともなってVersionも2.0.0から1.4.2に変更しました。
    それからVersionの数字の大きさを変えました
    前　： v1.0.4.1
    今回：v1.4.2

--- discordbot.py
+++ discordbot.py
@@ -112,27 +112,6 @@ async def on_message(message):
         await message.channel.send('ともぐいはしないよ！')
     if message.content == 'おかね':
         await message.channel.send('もぐもぐ...') 
-#Global Chat System    
-    GLOBAL_CH_NAME = "hotdog-global" # グローバルチャットのチャンネル名
-    GLOBAL_WEBHOOK_NAME = "hotdog-global-webhook" # グローバルチャットのWebhook名
-
-    if message.channel.name == GLOBAL_CH_NAME:
-        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
-        await message.delete()
-
-        channels = client.get_all_channels()
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-
-        for channel in global_channels:
-            ch_webhooks = await channel.webhooks()
-            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
-
-            if webhook is None:
-                # そのチャンネルに hotdog-global-webhook というWebhookは無かったので無視
-                continue
-            await webhook.send(content=message.content,
-                username=message.author.name,
-                avatar_url=message.author.avatar_url_as(format="png"))
     try:
             print(1 / 0)
     except : KeyError
